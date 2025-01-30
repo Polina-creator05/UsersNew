10fe703b96fe4c88fbdb68874890cd8e1b97d0ba
Polina <polinager502@gmail.com>
2025-01-30 17:47:50 +0300

    Add arthmetics action2

--- src/main/java/UserprojectExample/Test.java
+++ src/main/java/UserprojectExample/Test.java
@@ -4,6 +4,10 @@ public class Test {
     public static void main(final String[] args) {
         System.out.println(1 + 5);
 
-System.out.println("Hello!");
+        System.out.println("Hello!");
+
+        System.out.println(5 + 1);
+
+
     }
 }
