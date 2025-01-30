d7e22689a227c6f8133b8de525aa4887abffece9
Polina <polinager502@gmail.com>
2025-01-30 22:53:52 +0300

    Add dream drink

--- src/main/java/UserprojectExample/Test.java
+++ src/main/java/UserprojectExample/Test.java
@@ -12,7 +12,7 @@ public class Test {
 
         System.out.println("I am in loss");
         System.out.println("I am tired");
-        System.out.println("I want to travel");
+        System.out.println("I want to sleep and eat and drink");
 
     }
 }
