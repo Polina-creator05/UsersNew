f53b26d25fc1f63f720e2d417464ae839500ff6d
Polina <polinager502@gmail.com>
2025-01-30 11:24:09 +0300

    Delete Test2

--- src/main/java/UserprojectExample/Test2.java
+++
@@ -1,9 +0,0 @@
-package UserprojectExample;
-
-public class Test2 {
-    public static void main(final String[] args) {
-        System.out.println("Hello");
-        System.out.println("Bye");
-
-    }
-}
