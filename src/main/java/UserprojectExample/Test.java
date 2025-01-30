36aa68b7055c028df9d266b53f38703a00908c0b
Polina <polinager502@gmail.com>
2025-01-30 17:33:13 +0300

    Add Nononon

--- src/main/java/UserprojectExample/Test.java
+++ src/main/java/UserprojectExample/Test.java
@@ -3,6 +3,7 @@ package UserprojectExample;
 public class Test {
     public static void main(final String[] args) {
         System.out.println("Lalala");
+        System.out.println("Nonono");
 
     }
 }
