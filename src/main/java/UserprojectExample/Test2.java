b941bbfe2f8fe0bc5004c24c30faa28a03814079
Polina <polinager502@gmail.com>
2025-01-30 11:08:54 +0300

    Add farewell in Test2

--- src/main/java/UserprojectExample/Test2.java
+++ src/main/java/UserprojectExample/Test2.java
@@ -1,4 +1,9 @@
 package UserprojectExample;
 
 public class Test2 {
+    public static void main(final String[] args) {
+        System.out.println("Hello");
+        System.out.println("Bye");
+
+    }
 }
